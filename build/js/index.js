8d187b047a0d28e464b76f4be5ba59f111d82d5d
Andy Ford <andy.ford@Nology.local>
2019-04-02 11:20:23 +0100

    Fixed menu button and setup bundle

--- build/js/index.js
+++ build/js/index.js
@@ -1,4 +1,14 @@
-var toggleDropdown = function (e) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var newsData_1 = require("./newsData");
+var newsData = newsData_1.default;
+console.log("News data:", newsData);
+var addToPage = function (article) {
+    console.log(article.title);
+    console.log(article.source.name);
+};
+newsData.articles.forEach(addToPage);
+window.toggleDropdown = function (e) {
     e.preventDefault();
     document.getElementById('links').classList.toggle('hidden');
 };
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,37 @@
+import data from './newsData'
 
-let toggleDropdown = (e) => {
+interface INewsData {
+    status: string;
+    totalResults: number;
+    articles: IArticleData[];
+}
+
+interface IArticleData {
+    source: {
+        id: string,
+        name: string,
+    },
+    author: string,
+    title: string,
+    description: string,
+    url: string,
+    urlToImage: string,
+    publishedAt: string,
+    content: string,
+}
+
+const newsData: INewsData = data;
+console.log("News data:", newsData);
+
+let addToPage = (article:IArticleData) => {
+
+    console.log(article.title);
+    console.log(article.source.name);
+}
+
+newsData.articles.forEach(addToPage);
+
+(<any>window).toggleDropdown = (e: MouseEvent) => {
     e.preventDefault();
     document.getElementById('links').classList.toggle('hidden');
 }
