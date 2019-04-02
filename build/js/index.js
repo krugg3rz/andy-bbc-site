7b1c9756ebd18f2e019ab301e8bfbdc733d8e738
Andy Ford <andy.ford@Nology.local>
2019-04-02 11:44:47 +0100

    Fixed more stuff, got articles passing in

--- build/js/index.js
+++ build/js/index.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var newsData_1 = require("./newsData");
-var newsData = newsData_1.default;
-console.log("News data:", newsData);
-var addToPage = function (article) {
-    console.log(article.title);
-    console.log(article.source.name);
+var addToPage = function (articles) {
+    var html = "<article>\n                <img src=\"https://amp.businessinsider.com/images/5c952fe109b603212d46ae03-1136-568.png\"\n                    alt=\"A cat, many cats or a kitten\">\n                <div class=\"content\">\n                    <header>CME's CEO explains the biggest issue regulators have with bitcoin</header>\n                    <small>\n                        <span class=\"time\">14:50</span>\n                        <span class=\"source\">Business Insider</span>\n                    </small>\n                    <div class=\"description\">Lorem ipsum dolor amet cornhole edison bulb palo santo locavore banh mi migas,\n                        next\n                        level kombucha bushwick fixie shabby chic biodiesel knausgaard four dollar toast poutine. Organic\n                        four\n                        loko polaroid hashtag. Vaporware gluten-free tousled farm-to-table, 8-bit la croix deep v yuccie\n                        try-hard. Literally chicharrones lyft hammock, tumeric helvetica copper mug freegan blue bottle\n                        church-key vape ethical tilde everyday carry yuccie. Beard coloring book tofu scenester health goth.\n                    </div>\n                </div>\n            </article>";
+    document.getElementById('articles').innerHTML += html;
+    // console.log(document.getElementById('articles'));
 };
+var newsData = newsData_1.default;
+// console.log("News data:", newsData);
 newsData.articles.forEach(addToPage);
 window.toggleDropdown = function (e) {
     e.preventDefault();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,29 @@
 import data from './newsData'
 
+const addToPage = (articles: IArticleData) => {
+    let html = `<article>
+                <img src="https://amp.businessinsider.com/images/5c952fe109b603212d46ae03-1136-568.png"
+                    alt="A cat, many cats or a kitten">
+                <div class="content">
+                    <header>CME's CEO explains the biggest issue regulators have with bitcoin</header>
+                    <small>
+                        <span class="time">14:50</span>
+                        <span class="source">Business Insider</span>
+                    </small>
+                    <div class="description">Lorem ipsum dolor amet cornhole edison bulb palo santo locavore banh mi migas,
+                        next
+                        level kombucha bushwick fixie shabby chic biodiesel knausgaard four dollar toast poutine. Organic
+                        four
+                        loko polaroid hashtag. Vaporware gluten-free tousled farm-to-table, 8-bit la croix deep v yuccie
+                        try-hard. Literally chicharrones lyft hammock, tumeric helvetica copper mug freegan blue bottle
+                        church-key vape ethical tilde everyday carry yuccie. Beard coloring book tofu scenester health goth.
+                    </div>
+                </div>
+            </article>`;
+    document.getElementById('articles').innerHTML+=html;
+    // console.log(document.getElementById('articles'));
+}
+
 interface INewsData {
     status: string;
     totalResults: number;
@@ -21,13 +45,7 @@ interface IArticleData {
 }
 
 const newsData: INewsData = data;
-console.log("News data:", newsData);
-
-let addToPage = (article:IArticleData) => {
-
-    console.log(article.title);
-    console.log(article.source.name);
-}
+// console.log("News data:", newsData);
 
 newsData.articles.forEach(addToPage);
 
