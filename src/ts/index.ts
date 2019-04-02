a6fa8dbf325101fe3af66b25688abdad61a8c9bc
Andy Ford <andy.ford@Nology.local>
2019-04-02 12:30:33 +0100

    Placeholder images working

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,23 +1,21 @@
 import data from './newsData'
 
 const addToPage = (articles: IArticleData) => {
+    let image = (articles.urlToImage) ? articles.urlToImage: "../src/assets/placeholder-image.jpg";
     let html = `<article>
-                <img src="https://amp.businessinsider.com/images/5c952fe109b603212d46ae03-1136-568.png"
-                    alt="A cat, many cats or a kitten">
+                <div
+                class="image"
+                style="background-image:url('${image}')"></div>
                 <div class="content">
-                    <header>CME's CEO explains the biggest issue regulators have with bitcoin</header>
+                    <header>${articles.title}</header>
                     <small>
                         <span class="time">14:50</span>
                         <span class="source">Business Insider</span>
                     </small>
-                    <div class="description">Lorem ipsum dolor amet cornhole edison bulb palo santo locavore banh mi migas,
-                        next
-                        level kombucha bushwick fixie shabby chic biodiesel knausgaard four dollar toast poutine. Organic
-                        four
-                        loko polaroid hashtag. Vaporware gluten-free tousled farm-to-table, 8-bit la croix deep v yuccie
-                        try-hard. Literally chicharrones lyft hammock, tumeric helvetica copper mug freegan blue bottle
-                        church-key vape ethical tilde everyday carry yuccie. Beard coloring book tofu scenester health goth.
+                    <div class="description">
+                    ${articles.description}
                     </div>
+                    <a href="${articles.url}">Read more</a>
                 </div>
             </article>`;
     document.getElementById('articles').innerHTML+=html;
